0d75f47eb3139d746e22842712915b05c36d9006
Letisia Pangata'a <lpan375@aucklanduni.ac.nz>
2021-01-27 07:37:27 +1300

    final files

--- pages/about.js
+++ pages/about.js
@@ -1,30 +1,26 @@
 import Layout from '../components/Layout';
 import fetch from 'isomorphic-unfetch';
+import Error from './_error';
 import { Component } from 'react';
 
 export default class About extends Component {
     static async getInitialProps() {
         
         const res = await fetch('https://api.github.com/users/letisiapangataa');
+        const statusCode = res.status > 200 ? res.status : false;
         const data = await res.json();
 
-        return { user: data };
+
+
+        return { user: data, statusCode };
     }
-    
-    // componentDidMount() {
-    //     fetch('https://api.github.com/users/letisiapangataa')
-    //     .then(res => res.json())
-    //     .then(data => {
-    //         this.setState({
-    //             user: data
-    //         });
-    //     })
-    // }
 
     render() {
-        const { user } = this.props;
-    
-
+        const { user, statusCode } = this.props;
+        
+        if(statusCode) {
+            return <Error statusCode={statusCode} />
+        }
 
         return (
             <Layout title="About">
---
+++ pages/post.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+import { withRouter } from 'next/router';
+
+const Post = ({ router }) => (
+    <Layout title={router.query.title}>
+        <p style={{ width: "80vw" }}>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        </p>
+    </Layout>
+);
+
+export default withRouter(Post);
